720acdba56f5458f38c55bbcd9222d0f4961cbe8
Edwin Mwaniki <edwin.mwaniki@student.moringaschool.com>
2025-08-27 15:33:07 +0300

    Fix initial errors

---
+++ models.py
@@ -0,0 +1,47 @@
+from sqlalchemy import Integer, Float, String, DateTime, Text, create_engine, Column, ForeignKey, Boolean
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+from datetime import datetime
+
+Base = declarative_base()
+
+class Users(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable= False)
+    email = Column(String(50), nullable = False)
+
+    wallets = relationship('Wallets', back_populates = 'user')
+
+
+
+class Wallets(Base):
+    __tablename__ = "wallets"
+    id = Column(Integer, primary_key= True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable = False)
+    balance = Column(Float, nullable = False, default = 0.0)
+    
+    user = relationship('Users', back_populates = 'wallets')
+    sent_transactions= relationship('Transactions',back_populates = 'sender_wallet')
+    received_transactions = relationship('Transactions', back_populates = 'receiver_wallet')
+
+    
+
+class Transactions(Base):
+    __tablename__ = "transactions"
+    id = Column(Integer, primary_key = True)
+    sender_wallet_id = Column(Integer, ForeignKey("wallets.id"), nullable = False)
+    receiver_wallet_id = Column(Integer, ForeignKey("wallets.id"), nullable = False)
+    timestamp = Column(DateTime, nullable=False, default=datetime.utcnow)
+    description = Column(Text)
+    amount = Column(Float, nullable = False)
+
+    sender_wallet = relationship("Wallets", foreign_keys=[sender_wallet_id], back_populates = "sent_transactions")
+
+    receiver_wallet = relationship("Wallets", foreign_keys=[receiver_wallet_id], back_populates = "received_transactions")
+
+
+engine = create_engine("sqlite:///dwallet.db")
+Base.metadata.create_all(bind= engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
