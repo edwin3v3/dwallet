af9a5535a882fdbefdefe795acbc6d399fc8a525
Edwin Mwaniki <edwin.mwaniki@student.moringaschool.com>
2025-08-30 02:54:32 +0300

    Added new features

--- models.py
+++ models.py
@@ -6,37 +6,38 @@ Base = declarative_base()
 
 class Users(Base):
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True)
     name = Column(String(80), nullable= False)
     email = Column(String(50), nullable = False)
-
-    wallets = relationship('Wallets', back_populates = 'user')
+    wallets = relationship('Wallets', back_populates = 'users')
 
 
 
 class Wallets(Base):
     __tablename__ = "wallets"
+
     id = Column(Integer, primary_key= True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable = False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False, index=True)
     balance = Column(Float, nullable = False, default = 0.0)
     
-    user = relationship('Users', back_populates = 'wallets')
-    sent_transactions= relationship('Transactions',back_populates = 'sender_wallet')
-    received_transactions = relationship('Transactions', back_populates = 'receiver_wallet')
+    users = relationship('Users', back_populates = 'wallets')
+    sent_transactions= relationship('Transactions',back_populates = 'sender_wallet', foreign_keys = "Transactions.sender_wallet_id")
+    received_transactions = relationship('Transactions', back_populates = 'receiver_wallet', foreign_keys = "Transactions.receiver_wallet_id")
 
-    
+
 
 class Transactions(Base):
     __tablename__ = "transactions"
+    
     id = Column(Integer, primary_key = True)
-    sender_wallet_id = Column(Integer, ForeignKey("wallets.id"), nullable = False)
-    receiver_wallet_id = Column(Integer, ForeignKey("wallets.id"), nullable = False)
+    sender_wallet_id = Column(Integer, ForeignKey("wallets.id"), nullable=False, index=True)
+    receiver_wallet_id = Column(Integer, ForeignKey("wallets.id"), nullable=False, index=True)
     timestamp = Column(DateTime, nullable=False, default=datetime.utcnow)
     description = Column(Text)
     amount = Column(Float, nullable = False)
 
     sender_wallet = relationship("Wallets", foreign_keys=[sender_wallet_id], back_populates = "sent_transactions")
-
     receiver_wallet = relationship("Wallets", foreign_keys=[receiver_wallet_id], back_populates = "received_transactions")
 
 
